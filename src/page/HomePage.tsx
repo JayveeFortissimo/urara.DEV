43c5e522c6470b2a91609410199080e3b3e553f1
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-06-03 00:09:47 +0800

    Tools

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,6 +1,7 @@
 import image from "../assets/logo.png";
 import { MoveRight, ArrowRightToLine } from "lucide-react";
 import Myprojects from "../utils/Project";
+import Tools from "../utils/Tools";
 const HomePage = () => {
   return (
     <div className="min-h-[100vh] w-full flex flex-col justify-center items-center gap-[10rem] ">
@@ -22,35 +23,33 @@ const HomePage = () => {
         </button>
       </div>
 
-      {/* ===============================PROJECTS================================================================ */}
+{/* //PROJECTS//*/}
 
       <div className="w-[100%] min-h-[100vh] flex flex-col gap-10">
         <div className="h-[4rem] flex items-center justify-between flex-wrap">
           <div className="flex items-center gap-3">
             <p className="text-[2rem] text-white">
-              <span className="text-[#87CEEB] ">#</span>projects
+              <span className="text-[#87CEEB] ">#</span>Projects
             </p>
             <div className="h-[0rem] w-[25rem] border border-[#87CEEB] hidden sm:flex"></div>
           </div>
 
           <div className="flex flex-wrap items-center text-white gap-3">
-            <p className="text-[1rem]">View all</p>
+            <p className="text-[1rem] cursor-pointer">View all</p>
             <MoveRight />
           </div>
         </div>
-
-{/* //BODYY */}
 
 <div className="mi-h-[auto] w-full flex justify-center text-white gap-6 flex-wrap ">
 {
   Myprojects.map(pro => {
     return(
-      <div className="border border-white h-[30rem] flex flex-col justify-center items-center gap-4 flex-auto p-3">
+      <div className="border border-white h-[30rem] flex flex-col justify-center items-center gap-4 flex-auto p-3 rounded shadow-2xl">
             <img src={pro.image} className="h-[13rem]" />
-            <p>{pro.name}</p>
+            <p className="">{pro.name}</p>
             <p className="text-center">{pro.description}</p>
-            <div className="flex gap-2 border border-[#87CEEB] p-2 cursor-pointer hover:bg-blue-400">
-              <p>See</p>
+            <div className="flex items-center gap-2 border border-[#87CEEB] p-2 cursor-pointer hover:bg-blue-400">
+              <p className="text-[0.9rem]">See</p>
               <ArrowRightToLine />
             </div>
         </div>
@@ -58,10 +57,43 @@ const HomePage = () => {
   })
 }
 </div>
-
+   {/* // */}
         
+{/* MY TECHNOLOGIES */}
+     <div className="min-h-[50vh] flex flex-col gap-10">
+
+       <div className="h-[4rem] flex items-center justify-between flex-wrap">
+          <div className="flex items-center gap-3">
+            <p className="text-[2rem] text-white">
+              <span className="text-[#87CEEB] ">#</span>Tools
+            </p>
+            <div className="h-[0rem] w-[25rem] border border-[#87CEEB] hidden sm:flex"></div>
+          </div>
+
+          <div className="flex flex-wrap items-center text-white gap-3">
+            
+          </div>
+        </div>
+        {/*  */}
+<div className="h-auto p-3  flex justify-between items-center flex-wrap gap-3">
+{
+        Tools.map((pro,index)=> {
+          return (
+              <div key={index} >
+
+              <img src={pro.img} alt="" />
+        </div>
+          )
+        })
+      }
+</div>
+      
       </div>
-      {/* // */}
+
+{/* To outside this */}
+      </div>
+  
+      {/*  */}
     </div>
   );
 };
---
+++ src/utils/Tools.ts
@@ -0,0 +1,46 @@
+import {ToolsConfiguration} from '../../interface/tools.interface';
+
+
+const Tools: ToolsConfiguration[] = [
+    {
+        img:'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'
+    },
+    {
+        img:'https://img.shields.io/badge/css3%20-%2314354C.svg?&style=for-the-badge&logo=css3&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E'
+    },
+     {
+        img:'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
+    },
+     {
+        img:'https://img.shields.io/badge/node.js%20-%23008CC1.svg?&style=for-the-badge&logo=node.js&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/git%20-%23F05032.svg?&style=for-the-badge&logo=git&logoColor=white'
+    },
+     {
+        img:'http://img.shields.io/badge/-VS%20Code-000000?style=for-the-badge&logo=Visual-studio-code&logoColor=blue'
+    },
+     {
+        img:'https://img.shields.io/badge/Canva-%2300C4CC.svg?style=for-the-badge&logo=Canva&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/postgresql-%23336791.svg?style=for-the-badge&logo=postgresql&logoColor=white'
+    },
+     {
+        img:'https://img.shields.io/badge/nestjs-%23E0234E.svg?style=for-the-badge&logo=nestjs&logoColor=white'
+    },
+]
+
+export default Tools;
