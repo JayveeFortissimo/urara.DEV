5840ae4f45ceaa6c4f50d124201316e8adb8cee2
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-06-03 00:31:02 +0800

    About

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -23,12 +23,12 @@ const HomePage = () => {
         </button>
       </div>
 
-{/* //PROJECTS//*/}
+      {/* //PROJECTS//*/}
 
       <div className="w-[100%] min-h-[100vh] flex flex-col gap-10">
         <div className="h-[4rem] flex items-center justify-between flex-wrap">
           <div className="flex items-center gap-3">
-            <p className="text-[2rem] text-white">
+            <p className="text-[1.5rem] text-white">
               <span className="text-[#87CEEB] ">#</span>Projects
             </p>
             <div className="h-[0rem] w-[25rem] border border-[#87CEEB] hidden sm:flex"></div>
@@ -40,59 +40,77 @@ const HomePage = () => {
           </div>
         </div>
 
-<div className="mi-h-[auto] w-full flex justify-center text-white gap-6 flex-wrap ">
-{
-  Myprojects.map(pro => {
-    return(
-      <div className="border border-white h-[30rem] flex flex-col justify-center items-center gap-4 flex-auto p-3 rounded shadow-2xl">
-            <img src={pro.image} className="h-[13rem]" />
-            <p className="">{pro.name}</p>
-            <p className="text-center">{pro.description}</p>
-            <div className="flex items-center gap-2 border border-[#87CEEB] p-2 cursor-pointer hover:bg-blue-400">
-              <p className="text-[0.9rem]">See</p>
-              <ArrowRightToLine />
-            </div>
+        <div className="mi-h-[auto] w-full flex justify-center text-white gap-6 flex-wrap ">
+          {Myprojects.map((pro) => {
+            return (
+              <div className="border border-white h-[30rem] flex flex-col justify-center items-center gap-4 flex-auto p-3 rounded shadow-2xl">
+                <img src={pro.image} className="h-[13rem]" />
+                <p className="">{pro.name}</p>
+                <p className="text-center">{pro.description}</p>
+                <div className="flex items-center gap-2 border border-[#87CEEB] p-2 cursor-pointer hover:bg-blue-400">
+                  <p className="text-[0.9rem]">See</p>
+                  <ArrowRightToLine />
+                </div>
+              </div>
+            );
+          })}
         </div>
-    )
-  })
-}
-</div>
-   {/* // */}
-        
-{/* MY TECHNOLOGIES */}
-     <div className="min-h-[50vh] flex flex-col gap-10">
+        {/* // */}
 
-       <div className="h-[4rem] flex items-center justify-between flex-wrap">
-          <div className="flex items-center gap-3">
-            <p className="text-[2rem] text-white">
-              <span className="text-[#87CEEB] ">#</span>Tools
-            </p>
-            <div className="h-[0rem] w-[25rem] border border-[#87CEEB] hidden sm:flex"></div>
+        {/* MY TECHNOLOGIES */}
+        <div className="min-h-[50vh] flex flex-col gap-10">
+          <div className="h-[4rem] flex items-center justify-between flex-wrap">
+            <div className="flex items-center gap-3">
+              <p className="text-[1.5rem] text-white">
+                <span className="text-[#87CEEB] ">#</span>Tools
+              </p>
+              <div className="h-[0rem] w-[25rem] border border-[#87CEEB] hidden sm:flex"></div>
+            </div>
+
+            <div className="flex flex-wrap items-center text-white gap-3"></div>
           </div>
+          {/*  */}
+          <div className="h-auto p-3  flex justify-between items-center flex-wrap gap-3">
+            {Tools.map((pro, index) => {
+              return (
+                <div key={index}>
+                  <img src={pro.img} alt="" />
+                </div>
+              );
+            })}
+          </div>
+          {/* ABOUT ME */}
 
-          <div className="flex flex-wrap items-center text-white gap-3">
+          <div className="min-h-[50vh] flex flex-col gap-10">
+            <div className="h-[4rem] flex items-center justify-between flex-wrap">
+              <div className="flex items-center gap-3">
+                <p className="text-[1.5rem] text-white">
+                  <span className="text-[#87CEEB] ">#</span>About Me
+                </p>
+                <div className="h-[0rem] w-[25rem] border border-[#87CEEB] hidden sm:flex"></div>
+              </div>
+
+              <div className="flex flex-wrap items-center text-white gap-3"></div>
+            </div>
+            {/*  */}
+            <div className="min-h-[50vh] text-white flex flex-col gap-[2rem]">
+                <p>Hi! I'm Jayvee, a passionate Fullstack Developer and a soon-to-be graduate with a strong drive for building meaningful and user-focused digital experiences.</p>
+                <p>Over the past few years, I’ve been diving deep into both frontend and backend technologies—designing intuitive interfaces and developing efficient, scalable systems. Whether it’s crafting a clean UI or architecting a robust API, I enjoy the full spectrum of web development and love seeing a project come to life from start to finish.</p>
+                <p>I'm constantly learning and exploring new tools and frameworks to sharpen my skills and stay up to date with the ever-evolving tech landscape. My goal? To create impactful solutions that solve real problems and bring value to users.</p>
+            <p>When I'm not coding, you'll find me collaborating with teams, learning new technologies, or fine-tuning personal projects. I'm excited to grow professionally and make a mark in the tech industry.</p>
+            <p>Let’s build something amazing together!</p>
+            
+            </div>
             
           </div>
-        </div>
-        {/*  */}
-<div className="h-auto p-3  flex justify-between items-center flex-wrap gap-3">
-{
-        Tools.map((pro,index)=> {
-          return (
-              <div key={index} >
 
-              <img src={pro.img} alt="" />
+          {/* CONTACTS */}
+          
         </div>
-          )
-        })
-      }
-</div>
-      
+
+        {/* To outside this */}
       </div>
 
-{/* To outside this */}
-      </div>
-  
       {/*  */}
     </div>
   );
