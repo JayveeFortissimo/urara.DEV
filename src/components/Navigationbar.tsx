3fab531dbcf271f8a5300483197b1080f5ea294b
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-06-28 10:49:32 +0800

    fix: Navigation bar

--- src/components/Navigationbar.tsx
+++ src/components/Navigationbar.tsx
@@ -1,6 +1,6 @@
 import data from "../utils/NavData";
 import { Menu } from "lucide-react";
-import { useNavigate, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 interface props{
   handleClick: ()=> void
@@ -8,10 +8,10 @@ interface props{
 
 const Navigationbar: React.FC<props> = ({handleClick}) => {
 
-  const redirect = useNavigate();
+
 
   return (
-    <div className="min-h-[6rem] w-[100%] p-3 flex justify-between items-center sticky top-0">
+    <div className="min-h-[6rem] w-[100%] p-3 flex justify-between items-center sticky top-0 z-50">
       <p className="text-[#87CEEB] text-[1.1rem]">
         <span className="text-white">Urara.</span>DEV
       </p>
@@ -26,7 +26,7 @@ const Navigationbar: React.FC<props> = ({handleClick}) => {
         {data.map((pro, index) => {
           return (
             <div key={index}>
-              <NavLink className={`text-[${pro.color}] cursor-pointer`} onClick={() => redirect(`${pro.links}`)}>{pro.name}</NavLink>
+              <NavLink  to={pro.links} className={ ({isActive}) => `${isActive ? 'underline':undefined} text-[${pro.color}] cursor-pointer`}>{pro.name}</NavLink>
             </div>
           );
         })}
